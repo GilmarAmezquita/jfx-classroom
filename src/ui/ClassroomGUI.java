6182deb3dcc2a1cf3fbd6a00e0d76823600e25e3
Gilmar Amezquita <gilmar.amezquita1@correo.icesi.edu.co>
2021-02-13 01:51:39 -0500

    Modified panels and new methods for users login

--- src/ui/ClassroomGUI.java
+++ src/ui/ClassroomGUI.java
@@ -1,20 +1,33 @@
 package ui;
 
+import java.io.File;
 import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 import model.Classroom;
+import model.UserAccount;
 
 public class ClassroomGUI {
 	
@@ -28,23 +41,56 @@ public class ClassroomGUI {
     private PasswordField txtPasswordLogIn;
     
     @FXML
-    private DatePicker txtBirthday;
+    private Label lbAccountUsername;
 
+    @FXML
+    private ImageView userAvatar;
+    
+    @FXML
+    private TableView<UserAccount> tvAccountList;
+
+    @FXML
+    private TableColumn<UserAccount, String> tcUsername;
+
+    @FXML
+    private TableColumn<UserAccount, String> tcGender;
+
+    @FXML
+    private TableColumn<UserAccount, String>tcCareer;
+
+    @FXML
+    private TableColumn<UserAccount, String> tcBirthday;
+
+    @FXML
+    private TableColumn<UserAccount, String> tcBrowser;
+    
     @FXML
     private TextField txtUsernameSignIn;
-
+    
     @FXML
     private PasswordField txtPasswordSignIn;
-
+    
     @FXML
     private TextField txtImageRoute;
 
     @FXML
-    private ChoiceBox<String> cbFavoriteBrowser;
-
-    @FXML
     private ToggleGroup genderSelect;
     
+    @FXML
+    private CheckBox cbSoftwareEng;
+
+    @FXML
+    private CheckBox cbTelematicEng;
+
+    @FXML
+    private CheckBox cbIndustrialEng;
+    
+    @FXML
+    private DatePicker txtBirthday;
+
+    @FXML
+    private ChoiceBox<String> cbFavoriteBrowser;
+
 	private Classroom classroom;
     
 	public ClassroomGUI(Classroom classroom){
@@ -61,16 +107,12 @@ public class ClassroomGUI {
     }
     
     @FXML
-    public void accountLogIn(ActionEvent event) throws IOException {
+    public void accountLogIn(ActionEvent event) throws IOException{
     	boolean enterAccount;
     	if(!txtUsernameLogIn.getText().isEmpty() && !txtPasswordLogIn.getText().isEmpty()){
     		enterAccount = classroom.logInAccount(txtUsernameLogIn.getText(), txtPasswordLogIn.getText());
     		if(enterAccount) {
-        		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("account-list.fxml"));
-        		fxmlLoader.setController(this);
-        		Parent accountList= fxmlLoader.load();
-        		mainPanel.getChildren().clear();
-        		mainPanel.setCenter(accountList);
+    			showScreenAccountList(txtUsernameLogIn.getText());
     		}else {
     			incorrectInformation();
     		}
@@ -94,6 +136,29 @@ public class ClassroomGUI {
 		missingInformation.showAndWait();
     }
 
+    private void initializeTableView() {
+    	ObservableList<UserAccount> observableList;
+    	observableList = FXCollections.observableArrayList(classroom.getAccounts());
+		tvAccountList.setItems(observableList);
+		tcUsername.setCellValueFactory(new PropertyValueFactory<UserAccount,String>("username"));
+		tcGender.setCellValueFactory(new PropertyValueFactory<UserAccount,String>("gender")); 
+		tcCareer.setCellValueFactory(new PropertyValueFactory<UserAccount,String>("career"));
+		tcBirthday.setCellValueFactory(new PropertyValueFactory<UserAccount,String>("birthday"));
+		tcBrowser.setCellValueFactory(new PropertyValueFactory<UserAccount,String>("browser"));
+    }
+    
+    public void showScreenAccountList(String username)throws IOException{
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("account-list.fxml"));
+    	fxmlLoader.setController(this);
+    	Parent accountListPane = fxmlLoader.load();
+    	mainPanel.getChildren().clear();
+    	mainPanel.setCenter(accountListPane);
+    	lbAccountUsername.setText(username);
+    	Image avatar = new Image("file:///"+classroom.getUserAvatar(username),50,50,false,false);
+    	userAvatar.setImage(avatar);
+    	initializeTableView();
+    }
+    
     @FXML
     public void showScreenSignUp(ActionEvent event) throws IOException {
     	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("register.fxml"));
@@ -101,9 +166,40 @@ public class ClassroomGUI {
     	Parent signUpPane = fxmlLoader.load();
     	mainPanel.getChildren().clear();
     	mainPanel.setCenter(signUpPane);
+    	txtImageRoute.setEditable(false);
     	cbFavoriteBrowser.getItems().addAll("Chrome", "Opera", "FireFox", "Safari", "Edge", "Brave");
+    }    
+    
+    @FXML
+    void selectedSoftware(ActionEvent event) {
+    	cbTelematicEng.setSelected(false);
+    	cbIndustrialEng.setSelected(false);
+    }
+    
+    @FXML
+    void selectedTelematic(ActionEvent event) {
+    	cbSoftwareEng.setSelected(false);
+    	cbIndustrialEng.setSelected(false);
+    }
+    
+    @FXML
+    void selectedIndustrial(ActionEvent event) {
+    	cbSoftwareEng.setSelected(false);
+    	cbTelematicEng.setSelected(false);
     }
 
+    private String verifyCareer() {
+    	String career;
+    	if(cbSoftwareEng.isSelected()) {
+    		career = cbSoftwareEng.getText();
+    	}else if(cbTelematicEng.isSelected()) {
+    		career = cbTelematicEng.getText();
+    	}else {
+    		career = cbIndustrialEng.getText();
+    	}
+    	return career;
+    }
+    
     @FXML
     public void accountCreate(ActionEvent event) throws IOException {
     	boolean createAcc = checkForEmpty();
@@ -111,10 +207,12 @@ public class ClassroomGUI {
     	if(createAcc) {
     		String username = txtUsernameSignIn.getText();
     		String password = txtPasswordSignIn.getText();
+    		String imageRute = txtImageRoute.getText();
     		String gender = ((RadioButton) genderSelect.getSelectedToggle()).getText();
+    		String career = verifyCareer();
     		String birthday = txtBirthday.getValue().toString();
     		String browser = cbFavoriteBrowser.getValue().toString();
-    		created = classroom.createAccount(username, password, gender, null, birthday, browser);
+    		created = classroom.createAccount(username, password,imageRute, gender, career, birthday, browser);
     		alertAccount(created);
     	}
     }
@@ -136,6 +234,10 @@ public class ClassroomGUI {
     		create = false;
     		emptyAlert.setContentText("Gender is missing");
     		emptyAlert.showAndWait();
+    	}else if(!cbSoftwareEng.isSelected() && !cbTelematicEng.isSelected() && !cbIndustrialEng.isSelected()){
+    		create = false;
+    		emptyAlert.setContentText("Career is missing");
+    		emptyAlert.showAndWait();
     	}else if(txtBirthday.getValue() == null) {
     		create = false;
     		emptyAlert.setContentText("Birthday is missing");
@@ -147,7 +249,7 @@ public class ClassroomGUI {
     	}
     	return create;
     }
-    
+ 
     private void alertAccount(boolean created) throws IOException{
     	Alert alertCreated = new Alert(AlertType.INFORMATION);
     	alertCreated.setHeaderText(null);
@@ -165,6 +267,17 @@ public class ClassroomGUI {
 
     @FXML
     public void browseImageRoute(ActionEvent event) {
-
+    	FileChooser fileChooser = new FileChooser();
+    	fileChooser.setTitle("Open Image");
+    	fileChooser.getExtensionFilters().addAll(
+    			new FileChooser.ExtensionFilter("Png Files", "*.png")
+    			, new FileChooser.ExtensionFilter("Jpg Files","*.jpg"));
+    	 File selectedFile = fileChooser.showOpenDialog(null);
+    	 txtImageRoute.setText(selectedFile.getPath());
+    }
+    
+    @FXML
+    void accountLogOut(ActionEvent event) throws IOException {
+    	showScreenLogIn(null);
     }
 }
